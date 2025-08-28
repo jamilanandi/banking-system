9d3965f78beca8ba20f4583675f52eedae564e2f
jamilanandi <cse22-038@thuto.bac.ac.bw>
2025-08-28 23:24:47 +0200

    Update Account.java

--- src/src/Account.java
+++ src/src/Account.java
@@ -1,1 +1,33 @@
+public abstract class Account {
+    protected String accountNumber;
+    protected double balance;
+    protected String branch;
+
+    public Account(String accountNumber, double balance, String branch) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.branch = branch;
+    }
+
+    // Common deposit method
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited BWP " + amount + " into " + getAccountType());
+        }
+    }
+
+    // Abstract methods (must be implemented by subclasses)
+    public abstract void withdraw(double amount);
+    public abstract String getAccountType();
+
+    // Getters
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+}
 
