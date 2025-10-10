63b0e512cb4753a17c331c56c5cf65a6d04a7a6b
jamilanandi <cse22-038@thuto.bac.ac.bw>
2025-10-10 08:01:28 +0200

    Update Account.java

--- src/src/Account.java
+++ src/src/Account.java
@@ -1,33 +1,62 @@
+package banking.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Account {
     protected String accountNumber;
     protected double balance;
-    protected String branch;
+    protected String openingDate;
+    protected Customer customer;
+    protected List<Transaction> transactions;
+    protected double monthlyInterestRate;
 
-    public Account(String accountNumber, double balance, String branch) {
+    public Account(String accountNumber, Customer customer, double initialDeposit) {
         this.accountNumber = accountNumber;
-        this.balance = balance;
-        this.branch = branch;
-    }
-
-    // Common deposit method
-    public void deposit(double amount) {
-        if (amount > 0) {
-            balance += amount;
-            System.out.println("Deposited BWP " + amount + " into " + getAccountType());
+        this.customer = customer;
+        this.balance = initialDeposit;
+        this.transactions = new ArrayList<>();
+        this.openingDate = java.time.LocalDate.now().toString();
+        
+        // Record initial deposit
+        if (initialDeposit > 0) {
+            transactions.add(new Transaction("OPENING_DEPOSIT", initialDeposit, this.balance));
         }
     }
 
-    // Abstract methods (must be implemented by subclasses)
-    public abstract void withdraw(double amount);
-    public abstract String getAccountType();
-
     // Getters
-    public double getBalance() {
-        return balance;
+    public String getAccountNumber() { return accountNumber; }
+    public double getBalance() { return balance; }
+    public String getOpeningDate() { return openingDate; }
+    public Customer getCustomer() { return customer; }
+    public List<Transaction> getTransactions() { return transactions; }
+    public double getMonthlyInterestRate() { return monthlyInterestRate; }
+
+    // Core business methods
+    public void deposit(double amount) {
+        if (amount > 0) {
+            this.balance += amount;
+            transactions.add(new Transaction("DEPOSIT", amount, this.balance));
+            System.out.println("Deposited: BWP " + amount + " | New Balance: BWP " + this.balance);
+        }
     }
 
-    public String getAccountNumber() {
-        return accountNumber;
+    public abstract boolean withdraw(double amount);
+
+    public double calculateInterest() {
+        double interest = balance * monthlyInterestRate;
+        return interest;
+    }
+
+    public void applyMonthlyInterest() {
+        double interest = calculateInterest();
+        if (interest > 0) {
+            this.balance += interest;
+            transactions.add(new Transaction("INTEREST", interest, this.balance));
+        }
+    }
+
+    protected void addTransaction(String type, double amount) {
+        transactions.add(new Transaction(type, amount, this.balance));
     }
 }
-
